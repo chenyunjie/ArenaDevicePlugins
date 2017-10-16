cc50984b1bf632d61258908e3145d207c39e4fc9
陈云杰 <chenyunjie@qianmi.com>
2017-10-16 10:29:09 +0800

    修改为类方法

--- Sources/Device.swift
+++ Sources/Device.swift
@@ -2,13 +2,15 @@
 //  Device.swift
 //  ArenaDevicePlugins
 //
-//  Created by 陈云杰 on 2017/10/14.
+//  Created by 陈云杰 on 2017/10/16.
 //  Copyright © 2017年 江苏斑马软件技术有限公司. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
-//获取uuid值
-public func uuid() -> String {
-    return UUID().uuidString
+class Device: NSObject {
+
+    public class func uuid() -> String {
+        return UUID().uuidString
+    }
 }
