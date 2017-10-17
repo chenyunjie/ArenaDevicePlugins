215317812f1a25c81276d417859550cc7af37483
陈云杰 <chenyunjie@qianmi.com>
2017-10-17 08:41:16 +0800

    添加异步方法

--- Sources/Device.swift
+++ Sources/Device.swift
@@ -13,4 +13,13 @@ class Device: NSObject {
     public class func uuid() -> String {
         return UUID().uuidString
     }
+ 
+    public class func deviceInfo(_ data: Dictionary<String, Any>, _ callback: @escaping (_ result: Dictionary<String, Any>) -> Void) {
+        let deadLineTime = DispatchTime.now() + .seconds(1)
+        DispatchQueue.main.asyncAfter(deadline: deadLineTime) {
+            let result = ["result": "success"]
+            
+            callback(result)
+        }
+    }
 }
